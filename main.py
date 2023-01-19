d4fe08966f3d5d128843c2af7fff9cc3a57b70d6
Daniil Rudakov <rudakov.daniil.2004@mail.ru>
2023-01-19 14:39:11 +0300

    Added list of levels

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def draw_intro_screen(screen, i):
     """
     font = pygame.font.Font(None, 40)
     # Тексты
-    texts = ["Приветствую", "Это моя игра", "В ней нужно сбежать из школы"]
+    texts = ["Приветствую"]
     # Отрисовка нового текста и закрашивание старого
     if i < len(texts):
         screen.fill((0, 0, 0))
@@ -41,7 +41,7 @@ def draw_start_screen(screen):
     x1 = width // 2 - text1.get_width() // 2
     y1 = height // 2 - text1.get_height()
     w1 = text1.get_width()
-    h1= text1.get_height()
+    h1 = text1.get_height()
     screen.blit(text1, (x1, y1))
 
     # Кнопка правил
@@ -63,13 +63,68 @@ def draw_start_screen(screen):
 def draw_start_game(screen):
     """
     Экран - 2
-    Рисует экран игры
+    Рисует экран с набором уровней
     :param screen:
     :return:
     """
     screen.fill((0, 0, 0))
-    font = pygame.font.Font(None, 30)
-    text = font.render("Пока не готово", True, (0, 255, 0))
+    font = pygame.font.Font(None, 40)
+
+    level1 = font.render("Уровень 1 (Кабинет физики)", True, (0, 255, 0))
+    w1, h1 = level1.get_width(), level1.get_height()
+
+    level2 = font.render("Уровень 2 (Кабинет информатики)", True, (0, 255, 0))
+    w2, h2 = level2.get_width(), level2.get_height()
+
+    level3 = font.render("Уровень 3 (Кабинет математики)", True, (0, 255, 0))
+    w3, h3 = level3.get_width(), level3.get_height()
+
+    level4 = font.render("Уровень 4 (Кабинет русского языка)", True, (0, 255, 0))
+    w4, h4 = level4.get_width(), level4.get_height()
+
+    level5 = font.render("Уровень 5 (Коридор)", True, (0, 255, 0))
+    w5, h5 = level5.get_width(), level5.get_height()
+
+    x1 = width // 2 - w1 // 2
+    y1 = (height - h1 - 20 - h2 - 20 - h3 - 20 - h4 - 20 - h5) // 2
+
+    x2 = width // 2 - w2 // 2
+    y2 = y1 + h1 + 20
+
+    x3 = width // 2 - w3 // 2
+    y3 = y2 + h2 + 20
+
+    x4 = width // 2 - w4 // 2
+    y4 = y3 + h3 + 20
+
+    x5 = width // 2 - w5 // 2
+    y5 = y4 + h4 + 20
+
+    screen.blit(level1, (x1, y1))
+    screen.blit(level2, (x2, y2))
+    screen.blit(level3, (x3, y3))
+    screen.blit(level4, (x4, y4))
+    screen.blit(level5, (x5, y5))
+
+    pygame.draw.rect(screen, (0, 255, 0), (x1 - 5, y1 - 5, w1 + 10, h1 + 10), 1)
+    pygame.draw.rect(screen, (0, 255, 0), (x2 - 5, y2 - 5, w2 + 10, h2 + 10), 1)
+    pygame.draw.rect(screen, (0, 255, 0), (x3 - 5, y3 - 5, w3 + 10, h3 + 10), 1)
+    pygame.draw.rect(screen, (0, 255, 0), (x4 - 5, y4 - 5, w4 + 10, h4 + 10), 1)
+    pygame.draw.rect(screen, (0, 255, 0), (x5 - 5, y5 - 5, w5 + 10, h5 + 10), 1)
+
+    return x1, y1, w1, h1, x2, y2, w2, h2, x3, y3, w3, h3, x4, y4, w4, h4, x5, y5, w5, h5
+
+
+
+def draw_disabled_level(screen):
+    """
+    Показывает, что уровень недоступен, пока не пройден прошлый
+    :param screen:
+    :return:
+    """
+    screen.fill((0, 0, 0))
+    font = pygame.font.Font(None, 40)
+    text = font.render("Чтобы попасть на этот уровень, пройдите предыдущий", True, (0, 255, 0))
     x = width // 2 - text.get_width() // 2
     y = height // 2 - text.get_height() // 2
     screen.blit(text, (x, y))
@@ -93,21 +148,21 @@ def draw_how_to_play(screen):
 # Основа, так сказать, база, baseee
 if __name__ == '__main__':
     pygame.init()
-    size = width, height = 1200, 800
+    size = width, height = 1000, 600
     screen = pygame.display.set_mode(size)
     num_of_screen = 0 # Номер экрана
-    num_start_screen = 0 # Номер текста в начальном экране
+    num_start_screen = 0 # Номер текста в интро
 
     running = True
     while running:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT: # Выход при нажатии крестика
+            if event.type == pygame.QUIT: # Выход
                 running = False
-            if num_of_screen == 0:
+            if num_of_screen == 0: # Интро
                 draw_intro_screen(screen, num_start_screen)
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     num_start_screen += 1
-            if num_of_screen == 1:
+            if num_of_screen == 1: # Стартовый экран
                 x1, y1, w1, h1, x2, y2, w2, h2 = draw_start_screen(screen)
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     x, y = event.pos
@@ -115,12 +170,12 @@ if __name__ == '__main__':
                         num_of_screen = 2
                     elif x in range(x2, x2 + w2) and y in range(y2, y2 + h2):
                         num_of_screen = 3
-            if num_of_screen == 2:
-                draw_start_game(screen)
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_ESCAPE:
-                        num_of_screen = 1
-            if num_of_screen == 3:
+            if num_of_screen == 2: # Экран начать игру
+                x1, y1, w1, h1, x2, y2, w2, h2, x3, y3, w3, h3, x4, y4, w4, h4, x5, y5, w5, h5 = draw_start_game(screen)
+                if event.key == pygame.K_ESCAPE:
+                    num_of_screen = 1
+
+            if num_of_screen == 3: # Экран как играть
                 draw_how_to_play(screen)
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_ESCAPE:
